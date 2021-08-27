e1c3bc33c87da5c5537bc4d78f320819548a57fc
nokyung09 <87840094+nokyung09@users.noreply.github.com>
2021-08-27 09:26:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask, jsonify
 from flask import Flask, render_template, Response
 import cv2
 
+camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
+
 app = Flask(__name__)
 
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 data = [
         {
             "id": 1,
@@ -38,5 +51,15 @@ def get_api():
 def name():
     return "<font color=pink>จุฑารัตน์</font> <font color=purple>อยู่เจียม</font> <br> <font color=red>เลขที่9 ม.4/10</font> "
 
+
+
+@app.route('/video')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
